07b5a84abb6ce6cbcde63f58646917fab87a8ee1
Wagner Costa <silvacostawagner@gmail.com>
2025-12-12 14:46:26 -0300

    improving controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,97 +1,232 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IPlayerController
 {
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private CharacterProperties props;
+    [SerializeField] private CharacterProperties _stats;
+    private Rigidbody2D _rb;
+    private CapsuleCollider2D _col;
+    private FrameInput _frameInput;
+    private Vector2 _frameVelocity;
+    private bool _cachedQueryStartInColliders;
 
-    private Rigidbody2D rb;
-    private CapsuleCollider2D col;
-    private InputSystem_Actions actionMap;
-    private Vector2 velocity;
+    #region Interface
 
-    float horizontalInput;
-    private bool isGrounded;
-    private bool jumpHeld;
+    public Vector2 FrameInput => _frameInput.Move;
+    public event Action<bool, float> GroundedChanged;
+    public event Action Jumped;
 
-    private void Start()
+    #endregion
+
+    private float _time;
+    private InputSystem_Actions inputMap;
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<CapsuleCollider2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<CapsuleCollider2D>();
 
-        actionMap = new();
-        actionMap.Enable(); 
+        _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
+        _rb.gravityScale = 0f;
+        _rb.freezeRotation = true;
 
-        rb.gravityScale = 0f;
-        rb.freezeRotation = true;
+        inputMap = new();
+        inputMap.Enable();
+    }
 
-        isGrounded = true;
+    private void OnDestroy()
+    {
+        inputMap.Disable();
+        inputMap = null;
     }
 
     private void Update()
     {
-        CheckInputs();
+        _time += Time.deltaTime;
+        GatherInput();
+    }
+
+    private void GatherInput()
+    {
+        _frameInput = new FrameInput
+        {
+            JumpDown = inputMap.Player.Jump.WasPressedThisFrame(),
+            JumpHeld = inputMap.Player.Jump.IsPressed(),
+            Move = inputMap.Player.Move.ReadValue<Vector2>()
+        };
+
+        if (_stats.SnapInput)
+        {
+            _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
+            _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
+        }
+
+        if (_frameInput.JumpDown)
+        {
+            _jumpToConsume = true;
+            _timeJumpWasPressed = _time;
+        }
     }
 
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapBox(groundCheck.position, Vector2.one * props.groundCheckSize, 0f, props.groundMask);
-        velocity = rb.linearVelocity;
+        CheckCollisions();
 
         HandleJump();
-        AdjustGravity();
-        AdjustVelocity();
+        HandleDirection();
+        HandleGravity();
         
-        rb.linearVelocity = velocity;
+        ApplyMovement();
     }
 
-    private void CheckInputs()
+    #region Collisions
+    
+    private float _frameLeftGrounded = float.MinValue;
+    private bool _grounded;
+
+    private bool CapsuleCheck(Vector2 direction, float distance)
     {
-        horizontalInput = actionMap.Player.Move.ReadValue<Vector2>().x;
-        jumpHeld = actionMap.Player.Jump.IsPressed();
+        Vector2 worldSize = Vector2.Scale(_col.size, transform.lossyScale);
+        Vector2 point = _col.bounds.center;
+
+        // Move start point slightly opposite of cast direction to avoid starting inside colliders
+        point -= direction * 0.02f;  
+
+        return Physics2D.CapsuleCast(
+            point,
+            worldSize,
+            _col.direction,
+            0f,
+            direction,
+            distance,
+            _stats.GroundMask
+        );
     }
+
+    private void CheckCollisions()
+    {
+        Physics2D.queriesStartInColliders = false;
+
+        // Ground and Ceiling
+        bool groundHit = CapsuleCheck(Vector2.down, _stats.GrounderDistance);
+        bool ceilingHit = CapsuleCheck(Vector2.up, _stats.GrounderDistance);
+
+        // Hit a Ceiling
+        if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
+
+        // Landed on the Ground
+        if (!_grounded && groundHit)
+        {
+            _grounded = true;
+            _coyoteUsable = true;
+            _bufferedJumpUsable = true;
+            _endedJumpEarly = false;
+            GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
+        }
+        // Left the Ground
+        else if (_grounded && !groundHit)
+        {
+            _grounded = false;
+            _frameLeftGrounded = _time;
+            GroundedChanged?.Invoke(false, 0);
+        }
+
+        Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
+    }
+
+    #endregion
+
+
+    #region Jumping
+
+    private bool _jumpToConsume;
+    private bool _bufferedJumpUsable;
+    private bool _endedJumpEarly;
+    private bool _coyoteUsable;
+    private float _timeJumpWasPressed;
+
+    private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
+    private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
 
     private void HandleJump()
     {
-        if (!jumpHeld && !isGrounded) return;
-        velocity.y = props.jumpForce;
+        if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.linearVelocity.y > 0) _endedJumpEarly = true;
+
+        if (!_jumpToConsume && !HasBufferedJump) return;
+
+        if (_grounded || CanUseCoyote) ExecuteJump();
+
+        _jumpToConsume = false;
     }
 
-    private void AdjustGravity()
+    private void ExecuteJump()
     {
-        if (velocity.y > 0)
+        _timeJumpWasPressed = 0;
+        
+        _endedJumpEarly = false;
+        _bufferedJumpUsable = false;
+        _coyoteUsable = false;
+        
+        _frameVelocity.y = _stats.JumpPower;
+        
+        Jumped?.Invoke();
+    }
+
+    #endregion
+
+    #region Horizontal
+
+    private void HandleDirection()
+    {
+        if (_frameInput.Move.x == 0)
         {
-            float gravity = props.inAirGravity * (jumpHeld ? 1f : 3f);
-            velocity.y += gravity * Time.fixedDeltaTime;
+            var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
         }
         else
         {
-            velocity.y += props.baseGravity * Time.fixedDeltaTime;
+            _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
         }
-
-        if (velocity.y < props.maxFallSpeed) velocity.y = props.maxFallSpeed;
     }
 
+    #endregion
 
-    private void AdjustVelocity()
+    #region Gravity
+
+    private void HandleGravity()
     {
-        float speed = horizontalInput * props.moveSpeed;
-
-        if (horizontalInput != 0f)
+        if (_grounded && _frameVelocity.y <= 0f)
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, speed, props.acceleration * Time.deltaTime);
+            _frameVelocity.y = _stats.GroundingForce;
         }
         else
         {
-            velocity.x = Mathf.MoveTowards(velocity.x, 0f, props.deceleration * Time.deltaTime);
+            var inAirGravity = _stats.FallAcceleration;
+            if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
+            _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
         }
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        if (groundCheck == null) return;
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(groundCheck.position, Vector2.one * props.groundCheckSize);
-    }
+    #endregion
+
+    private void ApplyMovement() => _rb.linearVelocity = _frameVelocity;
 }
+
+public struct FrameInput
+{
+    public bool JumpDown;
+    public bool JumpHeld;
+    public Vector2 Move;
+}
+
+public interface IPlayerController
+{
+    public event Action<bool, float> GroundedChanged;
+
+    public event Action Jumped;
+    public Vector2 FrameInput { get; }
+}
+
+
+
